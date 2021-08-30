76a43ec7e3d0584f85dfc2483598dce22f503019
m1172 <muhshuh2000@gmail.com>
2021-08-30 12:49:07 +0200

    edited

---
+++ src/sidebar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class sidebar extends Component {
+  render() {
+    return (
+      <div className='Container'>
+        <div className='sidebar'></div>
+        <div className='body'></div>
+      </div>
+    );
+  }
+}
