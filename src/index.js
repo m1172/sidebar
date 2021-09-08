efb16fd41da142b5202293985425192b17f55dad
m1172 <muhshuh2000@gmail.com>
2021-09-08 12:32:57 +0200

    edited

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './sidebar';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
